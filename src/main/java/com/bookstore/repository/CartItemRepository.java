26c9c4f41b430d755571860b72dd694b077fc2bb
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-17 22:26:25 -0400

    Added backend

---
+++ src/main/java/com/bookstore/repository/CartItemRepository.java
@@ -0,0 +1,16 @@
+package com.bookstore.repository;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bookstore.domain.CartItem;
+import com.bookstore.domain.ShoppingCart;
+@Transactional
+public interface CartItemRepository extends CrudRepository<CartItem, Long> {
+	List<CartItem> findByShoppingCart(ShoppingCart shoppingCart);
+	
+//	List<CartItem> findByOrder(Order order);
+}
