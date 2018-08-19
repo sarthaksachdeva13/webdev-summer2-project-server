133c6fdead0be6c40fbd02f9c51cd17ef85e9880
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-19 11:36:37 -0400

    Tried redis configuration

--- src/main/java/com/bookstore/config/HttpSessionConfig.java
+++ src/main/java/com/bookstore/config/HttpSessionConfig.java
@@ -1,15 +1,31 @@
 package com.bookstore.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePool;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@Configuration
 @EnableRedisHttpSession
+@PropertySource("application.properties")
 public class HttpSessionConfig {
-	
-	@Bean
-	public LettuceConnectionFactory connectionFactory() {
-		return new LettuceConnectionFactory();
-	}
+
+
+    @Value("${spring.redis.host}")
+    private String redisHostName;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
+                redisHostName, redisPort);
+        return new LettuceConnectionFactory((LettucePool) redisStandaloneConfiguration);
+    }
 
 }
